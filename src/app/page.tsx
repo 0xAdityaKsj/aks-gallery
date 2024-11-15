1e0428d74e07db26b72425688863d1c34c94d4c0
Sai Jyothir Aditya Kantipudi <106384725+0xAdityaKsj@users.noreply.github.com>
2024-11-15 14:25:52 +0530

    remove db pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,11 @@ const mockImages = mockImageUrls.map((url, index) => (
 
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
 
 
   return (
 
     <main className="flex flex-wrap gap-4">
-      {
-        posts.map((post, index) => <div key={index}>{post.name}</div>)
-      }
       {
         [...mockImages, ...mockImages, ...mockImages].map((image, index) => (
           <img key={index} src={image.url} alt="" className="w-48 h-48 object-cover" />
